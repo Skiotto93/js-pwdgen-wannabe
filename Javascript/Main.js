70581e6e74827a3940e023d63431a15213a3131e
skiotto93 <fabio.forenza@hotmail.com>
2022-10-19 14:59:17 +0200

    Aggiunto un ; in Main.js

--- Javascript/Main.js
+++ Javascript/Main.js
@@ -7,4 +7,4 @@ const favoriteColor = prompt (`Digita il tuo colore preferito`);
 //Creo una variabile che contenga tutti e 3 i risultati
 const all = (firstName + lastName + favoriteColor);
 //Scrivi sulla pagina i risultati combinati + 76 (che sta per classe76)
-document.getElementById("password").innerHTML = (all + 76)
+document.getElementById("password").innerHTML = (all + 76);
