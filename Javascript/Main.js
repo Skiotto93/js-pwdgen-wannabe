6e562f283b805a069ee0cbcf34c79d29ae5f5083
skiotto93 <fabio.forenza@hotmail.com>
2022-10-20 18:15:33 +0200

    Correzione spaziature e parentesi

--- Javascript/Main.js
+++ Javascript/Main.js
@@ -1,10 +1,10 @@
 //Chiedi all’utente il suo nome
-const firstName = prompt (`Digita il tuo nome`);
+const firstName = prompt(`Digita il tuo nome`);
 //chiedi il cognome dell'utente
-const lastName = prompt (`Digita il tuo cognome`);
+const lastName = prompt(`Digita il tuo cognome`);
 //chiedi colore preferito dell'utente
-const favoriteColor = prompt (`Digita il tuo colore preferito`);
+const favoriteColor = prompt(`Digita il tuo colore preferito`);
 //Creo una variabile che contenga tutti e 3 i risultati
-const all = (firstName + lastName + favoriteColor);
+const all = firstName + lastName + favoriteColor;
 //Scrivi sulla pagina i risultati combinati + 76 (che sta per classe76)
-document.getElementById("password").innerHTML = (all + 76);
+document.getElementById("password").innerHTML = all + 76;
